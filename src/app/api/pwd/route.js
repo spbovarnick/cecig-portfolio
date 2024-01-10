9018b4fcd59ee2a93bbcf26b113d3da33b4df9b7
spbovarnick <spbovarnick@gmail.com>
2024-01-09 21:36:32 -0800

    Try catch in pwd

--- src/app/api/pwd/route.js
+++ src/app/api/pwd/route.js
@@ -8,26 +8,28 @@ export async function POST(request, params) {
     nda,
     password,
   }`
-  const cases = await sanityFetch({ query: query, qParams: {} })
-  const data = await request.json();
-  const enteredPwd = data.password;
-  const slug = data.slug;
-  const foundCase = cases.find(e => e.slug === slug)
-  const pwd = foundCase.password;
-  
-  const cookie = serialize(process.env.PASSWORD_COOKIE_NAME, "true", {
-    httpOnly: true,
-    path: `/${slug}`,
-  });
-
-  if (!bcrypt.compareSync(enteredPwd, pwd)) {
-    return new Response("Incorrect password", { status: 401 });
+  try {
+    const cases = await sanityFetch({ query: query, qParams: {} })
+    const data = await request.json();
+    const enteredPwd = data.password;
+    const slug = data.slug;
+    const foundCase = cases.find(e => e.slug === slug)
+    const pwd = foundCase.password;
+    const cookie = serialize(process.env.PASSWORD_COOKIE_NAME, "true", {
+      httpOnly: true,
+      path: `/${slug}`,
+    });
+    if (!bcrypt.compareSync(enteredPwd, pwd)) {
+      return new Response("Incorrect password", { status: 401 });
+    }
+    return new Response("Success", {
+      status: 200,
+      headers: {
+        "Set-Cookie": cookie,
+      },
+    })
+  } catch (error) {
+    console.error("Pwd validation error", error);
+    return new Response(error.message, { status: 500 });
   }
-  
-  return new Response("Success", {
-    status: 200,
-    headers: {
-      "Set-Cookie": cookie,
-    },
-  })
 }
