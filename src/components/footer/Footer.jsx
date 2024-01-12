e873c6fda45ed01a4be212efd99fb0db7692b497
spbovarnick <spbovarnick@gmail.com>
2024-01-12 10:38:26 -0800

    footer bg color changed

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,7 +4,7 @@ import './footer.css';
 export default function Footer(){
 
   return (
-    <div id="footer" className="box-border flex flex-col md:flex-row w-full md:justify-between md:items-center absolute bottom-0 bg-[#CECECE] p-6 md:p-0 md:h-[119px] leading-none text-2xl">
+    <div id="footer" className="box-border flex flex-col md:flex-row w-full md:justify-between md:items-center absolute bottom-0 p-6 md:p-0 md:h-[119px] leading-none text-2xl">
       <div id="CTAnect" className={`${bricolageGrotesque.className} font-bold text-[32px] mb-6 md:absolute md:mb-0 md:left-[38px]`}>
         Let&#39;s Connect! ✌️
       </div>
