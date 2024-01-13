6cf6e3c8c4f600b387c468a746f0d4f2838b0de4
spbovarnick <spbovarnick@gmail.com>
2024-01-13 11:22:55 -0800

    width fit for WorkUI and WritingUI

--- src/components/workUI/WorkUI.jsx
+++ src/components/workUI/WorkUI.jsx
@@ -12,8 +12,8 @@ export default function WorkUI({ slug, project, client, nda }) {
         id="link-cel"
         className="w-full h-full flex justify-between items-center font-semibold px-10 py-16 "
       >
-        <div className="w-1/2 text-[20px] sm:text-[32px]">{project}</div>
-        <div className={`${inter.className} w-1/2 text-right text-[20px] sm:text-[40px]`}>{client}</div>
+        <div className="w-fit text-[20px] sm:text-[32px]">{project}</div>
+        <div className={`${inter.className} ml-10 w-fit text-right text-[20px] sm:text-[40px]`}>{client}</div>
       </Link>
       <Link id="slide-cel" className="absolute top-0 w-full h-full" href={`/${slug}`}></Link>
     </div>
--- src/components/writingUI/WritingUI.jsx
+++ src/components/writingUI/WritingUI.jsx
@@ -10,8 +10,8 @@ export default function WritingUI({ link, title, host, pub }) {
         target="_blank"
         className=" w-full h-full flex justify-between items-center font-semibold px-10  py-16"
       >
-        <div className="w-1/2 text-[20px] sm:text-[32px]">{title}</div>
-        <div className={`${inter.className} w-1/2 text-right text-[20px] sm:text-[40px]`}>{pub}</div>
+        <div className="w-fit text-[20px] sm:text-[32px]">{title}</div>
+        <div className={`${inter.className} w-fit ml-10 text-right text-[20px] sm:text-[40px]`}>{pub}</div>
       </a>
       <a id="slide-cel" className="absolute top-0 w-full h-full" href={link} target="_blank"></a>
     </div>
