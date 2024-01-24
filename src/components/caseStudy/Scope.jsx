55e72c531f8897f92d5a01a23fcff1b9e9f1dc26
spbovarnick <spbovarnick@gmail.com>
2024-01-24 15:26:03 -0800

    flex-wrap in Scope.jsx

--- src/components/caseStudy/Scope.jsx
+++ src/components/caseStudy/Scope.jsx
@@ -4,7 +4,7 @@ export default function Scope({ deliverables }) {
   return (
     <section className="bg-[#E9E4E0] px-6 py-16">
       <p id="scope-title" className={`${bricolageGrotesque.className} text-[32px] text-black font-bold mb-6`}>SCOPE OF WORK</p>
-      <div className="grid grid-cols-1 gap-8 lg:grid-cols-none lg:flex">
+      <div className="grid grid-cols-1 gap-8 lg:grid-cols-none lg:flex lg:flex-wrap">
         { deliverables.map((deliverable, i ) => (
           <div key={i} className="flex items-center min-w-fit lg:mr-20 lg:last-of-type:mr-0">
             <div className={`${bricolageGrotesque.className} text-5xl text-black font-bold w-[76px] h-[76px] p-2 flex items-center justify-center bg-[#75FA4D] rounded-full mr-6`}>{i+1}</div>
