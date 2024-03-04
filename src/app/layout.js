f4ff260aed1423e91cb73dde678dfb0dc6272e2a
spbovarnick <spbovarnick@gmail.com>
2024-03-04 13:38:05 -0800

    metadataBase, twitter-image and og alts

--- src/app/layout.js
+++ src/app/layout.js
@@ -6,10 +6,11 @@ import Footer from '@/components/footer/Footer'
 export const metadata = {
   title: 'CECI GOMEZ',
   description: 'UX/UI Designer',
+  metadataBase: new URL("https://www.cecidoes.work")
 }
 
 export default function RootLayout({ children }) {
-  
+
   return (
     <html lang="en">
       <body className={`${manrope.className} relative pb-[220px] md:pb-[119px] min-h-screen`}>
